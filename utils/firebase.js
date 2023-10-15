d84cbb916a0b927af3e523dfebb77e79dd63e34d
Baitoey <you@example.com>
2023-10-15 18:30:34 +0700

    feat: deploy with firebase

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,10 +1,10 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyBD6_E-CZoUBVrBzLdf_MqY9AoiY8I2bAM",
-    authDomain: "netflix-build-89b86.firebaseapp.com",
-    projectId: "netflix-build-89b86",
-    storageBucket: "netflix-build-89b86.appspot.com",
-    messagingSenderId: "833402985122",
-    appId: "1:833402985122:web:da34f612dcf247e81495fb",
-    measurementId: "G-WTSSR1J783"
-  };
+  apiKey: "AIzaSyAW8HCWEXUm1YtRMabQ4awzSBRagczH258",
+  authDomain: "netflix-clone-62b9f.firebaseapp.com",
+  projectId: "netflix-clone-62b9f",
+  storageBucket: "netflix-clone-62b9f.appspot.com",
+  messagingSenderId: "482797363639",
+  appId: "1:482797363639:web:26d21f1b415d8604f64832",
+  measurementId: "G-DRQMSH4KL6",
+};
